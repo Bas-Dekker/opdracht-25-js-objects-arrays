8b902364fd55960f6a04621766ca52c81f3dc5da
Bas <bmjdekker@gmail.com>
2021-03-04 10:39:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+let person = {
+  name: "Bas",
+  age: "42",
+};
+
+//person.name = "John";
+
+//person["name"] = "Joop";
+
+console.log(person);
+
+let evaluation = {
+  value: "7, 8, 9",
+};
+
+console.log(evaluation);
+
+
+let colors = ["green", "blue", "red"];
+colors.push("yellow");
+colors.push("5");
+colors.push({ greeting: "hi ik ben een object" });
+
+//console.log(colors.length);
+//console.log(colors[2]);
+console.log(colors[5]);
+*/
